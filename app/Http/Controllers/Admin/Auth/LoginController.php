0c61d4ef7d54a5519a8ae7e2f6fcb7574949f7b7
Masum Rahman Hasan <55694939+Masumrahmanhasan@users.noreply.github.com>
2022-11-07 00:07:20 +0600

    implemented authentication for super admin

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\ValidationException;
+
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Default authentication view admin
+     *
+     * @return Application|Factory|View
+     */
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Authentication for admin with validation request
+     *
+     * @param LoginRequest $request
+     * @throws ValidationException
+     */
+    public function login(LoginRequest $request)
+    {
+
+        if(!$admin = $this->attemptLogin($request)) {
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
+        }
+
+
+        $this->generateToken($admin->id);
+
+        return redirect()->route('admin.dashboard');
+
+    }
+
+    /**
+     * Determine if the user credentials are correct
+     *
+     * @param Request $request
+     * @return false|Builder|Model|object
+     */
+    protected function attemptLogin(Request $request)
+    {
+        $admin = User::query()->where('email', $request->input('email'))
+                            ->where('role', 'admin')
+                            ->first();
+
+        if($admin && Hash::check($request->input('password'), $admin->password)) {
+
+            Auth::login($admin);
+            return $admin;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * generate unique token from id
+     *
+     * @param $id
+     * @return string
+     */
+    private function generateToken($id): string
+    {
+        $token = bcrypt(uniqid($id, true));
+
+        $admin = User::query()->find($id);
+        $admin->token = $token;
+        $admin->save();
+
+        return $token;
+    }
+
+
+}
