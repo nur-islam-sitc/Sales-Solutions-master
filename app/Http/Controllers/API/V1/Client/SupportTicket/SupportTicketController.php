263fe9e04a19da50ba9bea6221bbbc9571f22748
masumrahmanhasan <masumrahmanhasan@gmail.com>
2022-12-22 12:12:18 +0600

    attachment null bug fix, ticket list get by merchant fix

--- app/Http/Controllers/API/V1/Client/SupportTicket/SupportTicketController.php
+++ app/Http/Controllers/API/V1/Client/SupportTicket/SupportTicketController.php
@@ -5,21 +5,35 @@ namespace App\Http\Controllers\API\V1\Client\SupportTicket;
 use App\Http\Controllers\Controller;
 use App\Models\Attachment;
 use App\Models\SupportTicket;
+use App\Models\TicketComment;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\HigherOrderBuilderProxy;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class SupportTicketController extends Controller
 {
-    public function index(Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
     {
         $request->validate([
             'merchant_id' => 'required'
         ]);
-        $tickets = SupportTicket::query()->with('attachment', 'comments', 'comments.user', 'comments.attachment')->findOrFail($request->input('merchant_id'));
+        $tickets = SupportTicket::query()->with('attachment', 'comments', 'comments.user', 'comments.attachment')
+            ->where('user_id', $request->input('merchant_id'))
+            ->get();
         return response()->json($tickets);
     }
-    public function store(Request $request)
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
     {
         $request->validate([
             'merchant_id' => 'required',
@@ -27,44 +41,42 @@ class SupportTicketController extends Controller
             'content' => 'required|min:10'
         ]);
 
-        $attachment = null;
-
-        if($request->hasFile('attachment')) {
-            $fileExt = $request->file('attachment')->getClientOriginalExtension();
-            $size = $request->file('attachment')->getSize();
-            $type = $request->file('attachment')->getMimeType();
-
-            $path = '/upload/support-ticket';
-            $name = Carbon::now()->format('YmdHis') . '-' . uniqid() .'.'.$fileExt;
-            $image = $request->file('attachment')->storeAs($path, $name, 'local');
-
-            $attachment = Attachment::query()->create([
-                'name' => $name,
-                'type' => $type,
-                'size' => $size,
-                'path' => $image,
-            ]);
-        }
-
         $ticket = SupportTicket::query()->create([
             'user_id' => $request->input('merchant_id'),
             'ticket_id' => mt_rand(11111, 99999),
             'subject' => $request->input('subject'),
             'content' => $request->input('content'),
-            'attachment_id' => $attachment->id ?: null,
         ]);
 
-        $ticket->load('attachment');
+        if ($request->hasFile('attachment')) {
+            $attachment = $this->uploadAttachment($request);
+            $ticket->update(['attachment_id' => $attachment]);
+            $ticket->load('attachment');
+        }
+
         return response()->json($ticket);
     }
 
-    public function show($merchant, $id)
+    /**
+     * @param $merchant
+     * @param $id
+     * @return JsonResponse
+     */
+    public function show($merchant, $id): JsonResponse
     {
-        $ticket = SupportTicket::query()->with('attachment')->where('user_id', $merchant)->where('id', $id)->first();
+        $ticket = SupportTicket::query()->with('attachment')
+            ->where('user_id', $merchant)
+            ->where('id', $id)
+            ->first();
         return response()->json($ticket);
     }
 
-    public function reply(Request $request, $id)
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function reply(Request $request, $id): JsonResponse
     {
         $request->validate([
             'merchant_id' => 'required',
@@ -72,35 +84,47 @@ class SupportTicketController extends Controller
         ]);
         $ticket = SupportTicket::query()->where('user_id', $request->input('merchant_id'))->where('id', $id)->first();
 
-        if(!$ticket) {
+        if (!$ticket) {
             return response()->json(['message' => 'No Ticket Found, Please Select Valid Ticket Id']);
         }
-        $attachment = null;
-        if($request->hasFile('attachment')) {
 
-            $fileExt = $request->file('attachment')->getClientOriginalExtension();
-            $size = $request->file('attachment')->getSize();
-            $type = $request->file('attachment')->getMimeType();
-
-            $path = '/upload/support-ticket';
-            $name = Carbon::now()->format('YmdHis') . '-' . uniqid() .'.'.$fileExt;
-            $image = $request->file('attachment')->storeAs($path, $name, 'local');
-
-            $attachment = Attachment::query()->create([
-                'name' => $name,
-                'type' => $type,
-                'size' => $size,
-                'path' => $image,
-            ]);
-        }
-        $ticket->comments()->create([
-            'id' => Str::orderedUuid()->toString(),
+        $comment = TicketComment::query()->create([
+            'ticket_id' => $ticket->id,
             'user_id' => $request->input('merchant_id'),
             'content' => $request->input('content'),
-            'attachment_id' => $attachment->id ?: null,
         ]);
-        $ticket->load(['attachment', 'comments', 'comments.attachment', 'comments.user']);
 
-        return response()->json($ticket);
+        if ($request->hasFile('attachment')) {
+            $attachment = $this->uploadAttachment($request);
+            $comment->update(['attachment_id' => $attachment]);
+        }
+
+        $comment->load(['attachment', 'user']);
+
+        return response()->json($comment);
+    }
+
+    /**
+     * @param $request
+     * @return HigherOrderBuilderProxy|mixed
+     */
+    public function uploadAttachment($request)
+    {
+        $fileExt = $request->file('attachment')->getClientOriginalExtension();
+        $size = $request->file('attachment')->getSize();
+        $type = $request->file('attachment')->getMimeType();
+
+        $path = '/upload/support-ticket';
+        $name = Carbon::now()->format('YmdHis') . '-' . uniqid() . '.' . $fileExt;
+        $image = $request->file('attachment')->storeAs($path, $name, 'local');
+
+        $attachment = Attachment::query()->create([
+            'name' => $name,
+            'type' => $type,
+            'size' => $size,
+            'path' => $image,
+        ]);
+
+        return $attachment->id;
     }
 }
