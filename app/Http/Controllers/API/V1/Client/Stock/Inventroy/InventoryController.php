b33aca40c13cc03576a6d534a9b29e7fdf0e0fb3
samun.aleshasolutions <samun.aleshasolutions@gmail.com>
2022-12-22 14:37:31 +0600

    working on stock

---
+++ app/Http/Controllers/API/V1/Client/Stock/Inventroy/InventoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Client\Stock\Inventroy;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InventoryController extends Controller
+{
+    public function index()
+    {
+        # code...
+    }
+}
---
+++ app/Http/Controllers/API/V1/Client/Stock/StockIn/StockInController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Client\Stock\StockIn;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StockInController extends Controller
+{
+    //
+}
