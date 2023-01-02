356ea3f6d60c03ca93510295dda7024e9319e9d1
masumrahmanhasan <masumrahmanhasan@gmail.com>
2023-01-02 15:11:27 +0600

    categories api updated

--- app/Http/Controllers/API/V1/Customer/CategoryController.php
+++ app/Http/Controllers/API/V1/Customer/CategoryController.php
@@ -4,29 +4,26 @@ namespace App\Http\Controllers\API\V1\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Traits\sendApiResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    use sendApiResponse;
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        try {
-            $category  = Category::with('category_image')->get();
-            return response()->json([
-                'success' => true,
-                'data' => $category,
-            ]);
-        } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'msg' =>  $e->getMessage(),
-            ], 400);
+        $categories = Category::query()->with('category_image')->get();
+        if($categories->isEmpty()) {
+            return $this->sendApiResponse('', 'No data available',);
         }
+        return $this->sendApiResponse($categories, 'No data available',);
+
     }
 
 
