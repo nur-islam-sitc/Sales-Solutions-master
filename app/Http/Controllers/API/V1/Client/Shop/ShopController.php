526fed8da026d8b1ab55c178463defd7c9a5ce20
masumrahmanhasan <masumrahmanhasan@gmail.com>
2023-01-03 15:15:57 +0600

    updated shop info api

--- app/Http/Controllers/API/V1/Client/Shop/ShopController.php
+++ app/Http/Controllers/API/V1/Client/Shop/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\V1\Client\Shop;
 
 use App\Http\Controllers\MerchantBaseController;
+use App\Models\ActiveTheme;
 use App\Models\Shop;
 use App\Traits\sendApiResponse;
 use Illuminate\Http\JsonResponse;
@@ -15,12 +16,22 @@ class ShopController extends MerchantBaseController
 
     public function index(Request $request): JsonResponse
     {
-        if($request->header('domain') && $request->header('domain') !== null) {
+        if ($request->header('domain') && $request->header('domain') !== null) {
             $shop = Shop::query()->where('domain', $request->header('domain'))->first();
             if (!$shop) {
                 throw ValidationException::withMessages([
                     'shop_id' => 'domain not found'
                 ]);
+            }
+
+            $activeTheme = ActiveTheme::query()->join('themes', 'themes.id', '=', 'active_themes.theme_id')
+                ->where('active_themes.shop_id', $shop->shop_id)
+                ->where('active_themes.type', 'multiple')
+                ->first();
+
+            $shop['theme_id'] = NULL;
+            if ($activeTheme) {
+                $shop['theme_id'] = $activeTheme->name;
             }
 
             $shop->load('shop_logo', 'merchant');
