1cf7e367ce92c2643a64d181fbb17b3ba0ef51d3
masumrahmanhasan <masumrahmanhasan@gmail.com>
2023-01-09 13:20:58 +0600

    added countdown

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+if(Carbon::now()->setTimezone('Asia/Dhaka') <= Carbon::create(env('OFFLINE_YEAR'),env('OFFLINE_MONTH'),env('OFFLINE_DAY'),env('OFFLINE_HOURS'),env('OFFLINE_MINUTES'),env('OFFLINE_SECONDS'))->setTimezone('Asia/Dhaka')) {
+    Route::any('{query}', function () {
+        return view('comingsoon::comingsoon');
+    })->where('query', '.*');
+}
+
 
 Route::get('/', [\App\Http\Controllers\General\HomeController::class, 'index'])->name('home');
-
 Route::get('/signup', [\App\Http\Controllers\Merchant\Auth\LoginController::class, 'index'])->name('merchant.register');
 Route::post('/signup/store', [\App\Http\Controllers\Merchant\Auth\LoginController::class, 'register'])->name('merchant.register.store');
 Route::get('/thank_you', [\App\Http\Controllers\General\HomeController::class, 'thankYou'])->name('thank_you');
